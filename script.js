43d82f60102bd4d31ad9c57ab8b5a15c599100ff
jerichodgs <sinenengjericho0515@gmail.com>
2025-11-02 09:31:49 +0800

    bug fixes
    scrolling animation bug fixed

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ if (mobileToggle) {
     setTheme(mobileToggle.checked);
   });
 }
+
+// Add smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
